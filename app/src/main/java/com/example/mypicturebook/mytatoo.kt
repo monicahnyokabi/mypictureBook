d395f89bcc21386d2c0d0c84dad7f5e86c77db01
monicahnyokabi <wambuinyokabimn@gmail.com>
2020-08-01 00:58:47 +0300

    first commit

---
+++ app/src/main/java/com/example/mypicturebook/mytatoo.kt
@@ -0,0 +1,12 @@
+package com.example.mypicturebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class mytatoo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mytatoo)
+    }
+}
---
+++ app/src/main/java/com/example/mypicturebook/tatoo4.kt
@@ -0,0 +1,12 @@
+package com.example.mypicturebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tatoo4 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tatoo4)
+    }
+}
